0701f2cfca1597d273d4173312ea62fa7056a570
allanShady <acamilo.ag@gmail.com>
2021-02-24 01:34:55 +0200

    feat: isolate experience bar css

--- source/moveit-next/src/components/ExperienceBar.tsx
+++ source/moveit-next/src/components/ExperienceBar.tsx
@@ -1,6 +1,8 @@
+import styles from '../styles/components/ExperienceBar.module.css';
+
 export function ExperienceBar() {
     return(
-        <header className="experience-bar">
+        <header className={styles.experienceBar}>
             <span>0 px</span>
             <div>
                 <div style={{ width: '50%' }} />
