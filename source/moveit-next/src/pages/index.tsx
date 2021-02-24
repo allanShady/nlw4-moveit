96e03d37d4f26ff6a1aab8534896c6d83633b9f9
allanShady <acamilo.ag@gmail.com>
2021-02-24 03:07:05 +0200

    feat: add components main

--- source/moveit-next/src/pages/index.tsx
+++ source/moveit-next/src/pages/index.tsx
@@ -1,10 +1,28 @@
 import Head from 'next/head'
+import { CompletedChallenges } from '../components/CompletedChallenges'
+import { CountDown } from '../components/CountDown'
 import { ExperienceBar } from '../components/ExperienceBar'
+import { Profile } from '../components/Profile'
+import styles from '../styles/pages/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
+      <Head>
+        <title>Start move.it</title>
+      </Head>
+
       <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <CountDown />
+        </div>
+        
+        <div></div>
+      </section>
     </div>
   )
 }
