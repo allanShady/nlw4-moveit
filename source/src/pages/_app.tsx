3b85c1ffaa09eeee7e8db10b08c15d2a3c96e959
allanShady <acamilo.ag@gmail.com>
2021-02-25 08:05:03 +0200

    feat: setup challenge context provider

--- source/src/pages/_app.tsx
+++ source/src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import '../styles/global.css'
+import { ChallengesContextProvider } from '../contexts/ChallengesContext'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChallengesContextProvider> 
+      <Component {...pageProps} />
+    </ChallengesContextProvider>
+  )
 }
 
 export default MyApp
