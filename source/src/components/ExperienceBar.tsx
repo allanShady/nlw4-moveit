112173e07487984aea1fee63552db048ce768d54
allanShady <acamilo.ag@gmail.com>
2021-02-25 06:53:17 +0200

    fix: typo

--- source/src/components/ExperienceBar.tsx
+++ source/src/components/ExperienceBar.tsx
@@ -3,14 +3,14 @@ import styles from '../styles/components/ExperienceBar.module.css';
 export function ExperienceBar() {
     return(
         <header className={styles.experienceBar}>
-            <span>0 px</span>
+            <span>0 xp</span>
             <div>
                 <div style={{ width: '50%' }} />
                 <span className="current-experience" style={{ left: '50%' }}>
-                    300 px
+                    300 xp
                 </span>
             </div>
-            <span>600px</span>
+            <span>600 xp</span>
         </header>
     )
 }
