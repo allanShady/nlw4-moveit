307e7ac57ff13a7111be2a1134b317fe3dd5499b
allanShady <acamilo.ag@gmail.com>
2021-02-27 06:27:44 +0200

    feat: get data from cookies using the SSR

--- source/src/pages/index.tsx
+++ source/src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import { GetServerSideProps } from 'next'
+
 import { ChallengeBox } from '../components/ChallengeBox'
 import { CompletedChallenges } from '../components/CompletedChallenges'
 import { CountDown } from '../components/CountDown'
@@ -6,9 +8,21 @@ import { ExperienceBar } from '../components/ExperienceBar'
 import { Profile } from '../components/Profile'
 import { CountDownProvider } from '../contexts/CountDownContext'
 import styles from '../styles/pages/Home.module.css'
+import { ChallengesContextProvider } from '../contexts/ChallengesContext'
 
-export default function Home() {
+interface HomeProps {
+  level: number
+  currentExperience: number 
+  completedChallenges: number
+}
+
+export default function Home(props: HomeProps) {
   return (
+    <ChallengesContextProvider 
+      level={props.level}
+      currentExperience={props.currentExperience}
+      completedChallenges={props.completedChallenges}
+      > 
     <div className={styles.container}>
       <Head>
         <title>Start move.it</title>
@@ -30,5 +44,18 @@ export default function Home() {
         </section>
       </CountDownProvider>
     </div>
+    </ChallengesContextProvider>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) =>  {
+
+  const { level, currentExperience, completedChallenges } = ctx.req.cookies
+
+  return { 
+    props: {
+      level: Number(level), 
+      currentExperience: Number(currentExperience), 
+      completedChallenges: Number(completedChallenges)
+    } }
 }
